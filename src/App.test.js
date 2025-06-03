a56d37f48ccecd1dcac81d43827ee5ea3e4519dd
Shadman Ahmed <61790489+shadman-a@users.noreply.github.com>
2025-06-03 23:55:57 +0000

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders add button', () => {
-  render(<App />);
-  const addButton = screen.getByRole('button', { name: /add/i });
-  expect(addButton).toBeInTheDocument();
-});
